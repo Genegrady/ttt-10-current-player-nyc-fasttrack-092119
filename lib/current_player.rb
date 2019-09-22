dd8dbe6ea8cb42ab827f75e455c7e2925333f747
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-22 16:32:36 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,3 +6,15 @@ def turn_count(board)
     end
     
   end
+  return X
+end
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 || turn_count(board) == 0 
+      return "X"
+  else
+      return "O"
+  end
+  
+end
